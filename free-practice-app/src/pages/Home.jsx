18e8491f367a1fb7883c50e431221c245abcca08
akihiko-f <dfa2wad3fd9df4gewr5@gmail.com>
2025-10-29 21:55:56 +0900

    Home.jsx追加

---
+++ free-practice-app/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import HeroSection from "../components/sections/HeroSection";
+import FeatureSection from "../components/sections/FeatureSection";
+import PricingSection from "../components/sections/PricingSection";
+import CTASection from "../components/sections/CTASection";
+
+function Home() {
+    return (
+        <>
+            <HeroSection />
+            <FeatureSection />
+            <PricingSection />
+            <CTASection />
+        </>
+    )
+}
+
+export default Home
