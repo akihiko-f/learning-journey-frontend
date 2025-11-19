20b32229d5dc86bc4f7abe75b1dbf8b87c7b8f02
akihiko-f <dfa2wad3fd9df4gewr5@gmail.com>
2025-11-19 22:28:02 +0900

    テキストボックスを追加

--- frontend/free-practice-app/src/pages/Contact.jsx
+++ frontend/free-practice-app/src/pages/Contact.jsx
@@ -3,6 +3,7 @@ function Contact() {
         <div className="page-container">
             <h1>お問い合わせ</h1>
             <p>お気軽にお問い合わせください</p>
+            <input type="text" className="form" />
         </div>
     )
 }
