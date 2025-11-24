bac1afb4de5d8620a1458accec7f460fb22004b4
akihiko-f <dfa2wad3fd9df4gewr5@gmail.com>
2025-11-24 21:57:39 +0900

    feat: NotFoundページ追加

---
+++ frontend/portfolio-app/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+        <div className="page">
+            <h1>404 - Page Not Found</h1>
+            <p>お探しのページが見つかりませんでした。</p>
+        </div>
+    )
+}
+
+export default NotFound
