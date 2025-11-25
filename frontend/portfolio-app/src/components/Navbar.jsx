60f4b90cfc34e339da145298ac7264bdabf4e2c0
akihiko-f <dfa2wad3fd9df4gewr5@gmail.com>
2025-11-25 22:15:36 +0900

    feat: 各ページへのルーティング追加

---
+++ frontend/portfolio-app/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <nav className='navbar'>
+            <div className='nav-container'>
+                <Link to="/" className='nav-logo'>
+                    MyPortfolio
+                </Link>
+
+                <ul className='nav-menu'>
+                    <li className='nav-item'>
+                        <Link to="/" className='nav-link'>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to="/about" className='nav-link'>
+                            About
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to="/projects" className='nav-link'>
+                            Projects
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to="/contact" className='nav-link'>
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
